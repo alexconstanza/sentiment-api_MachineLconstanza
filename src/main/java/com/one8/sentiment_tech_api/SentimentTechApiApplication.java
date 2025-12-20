090fba76e2df03a632504d7fefc20f50adac3062
WilmarDeML <carewaz23@hotmail.com>
2025-12-19 20:46:07 -0500

    inicia el proyecto

---
+++ src/main/java/com/one8/sentiment_tech_api/SentimentTechApiApplication.java
@@ -0,0 +1,13 @@
+package com.one8.sentiment_tech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentTechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentTechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one8/sentiment_tech_api/SentimentTechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one8.sentiment_tech_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentTechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
